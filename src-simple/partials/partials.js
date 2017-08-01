cbd145fe8092e89eca93c650d16a4326495c8116
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-08-01 17:27:19 +0200

    -getting an external partial

--- src-simple/partials/partials.js
+++ src-simple/partials/partials.js
@@ -6,9 +6,18 @@ var template = document.getElementById('app-template').innerHTML;
 var partial = document.getElementById('app-partial').innerHTML;
 var compiled = Handlebars.compile(template);
 
+//
 // must register before render
+
 Handlebars.registerPartial('partial', partial);
 Handlebars.registerPartial('partialToo', 'partial from inline template {{name}}');
 
-// render
-document.getElementById('app').innerHTML = compiled(data);
+axios.get('partial.hbs')
+    .then(function (response) {
+        Handlebars.registerPartial('externalPartial', response.data);
+
+        // render
+        document.getElementById('app').innerHTML = compiled(data);
+    });
+
+
