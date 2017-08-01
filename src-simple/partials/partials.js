27fb1186d94ff0b73fd016a261e6b8fc7613d8e0
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-08-01 17:09:24 +0200

    -partials

--- src-simple/partials/partials.js
+++ src-simple/partials/partials.js
@@ -8,6 +8,7 @@ var compiled = Handlebars.compile(template);
 
 // must register before render
 Handlebars.registerPartial('partial', partial);
+Handlebars.registerPartial('partialToo', 'partial from inline template {{name}}');
 
 // render
 document.getElementById('app').innerHTML = compiled(data);
