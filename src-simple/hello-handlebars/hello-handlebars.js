f75d97df03f5f68b2f7f3eec066f40d3cedf159a
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-08-01 12:19:20 +0200

    -compile with options

--- src-simple/hello-handlebars/hello-handlebars.js
+++ src-simple/hello-handlebars/hello-handlebars.js
@@ -1,4 +1,6 @@
-// Handlebars.compile returns a function that can be executed with data, it can be run multiple times
+// Handlebars.compile takes a template string and options (optional)
+// Handlebars.compile returns a function that can be executed with data (context), it can be run multiple times
+
 var template = Handlebars.compile('<div>Hello {{name}}</div>');
 
 var $app = document.getElementById('app');
@@ -6,3 +8,9 @@ $app.innerHTML = template({ name: 'Foo' });
 
 var $appToo = document.getElementById('app-too');
 $appToo.innerHTML = template({ name: 'Bar' });
+
+// Handlebars.compile using options
+
+var templateSpicy = Handlebars.compile('<div>{{itsAlive}}</div>', { noEscape: true });
+var $appSpicy = document.getElementById('app-spicy');
+$appSpicy.innerHTML = templateSpicy({ itsAlive: '<i>Hi there</i>' });
