330b7f59e8c81c3762d76dd34cbca06e61558da2
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-08-01 17:30:34 +0200

    -comments

--- src-simple/hello-handlebars/hello-handlebars.js
+++ src-simple/hello-handlebars/hello-handlebars.js
@@ -9,9 +9,9 @@ $app.innerHTML = template({ name: 'Foo' });
 var $appToo = document.getElementById('app-too');
 $appToo.innerHTML = template({ name: 'Bar' });
 
-// Handlebars.compile using options
+// Handlebars.compile using options (render html as non escaped)
 
-var templateSpicy = Handlebars.compile('<div>{{itsAlive}}</div>', { noEscape: true });
+var templateSpicy = Handlebars.compile('<div>{{itsAlive}}</div>', { noEscape: true }); // could also use {{{itsAlive}}} (3 curly braces)
 var $appSpicy = document.getElementById('app-spicy');
 $appSpicy.innerHTML = templateSpicy({ itsAlive: '<i>Hi there</i>' });
 
