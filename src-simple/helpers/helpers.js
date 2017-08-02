a6425f43985b05952841e69c82f27075c985ae45
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-08-02 11:45:34 +0200

    -helpers

--- src-simple/helpers/helpers.js
+++ src-simple/helpers/helpers.js
@@ -20,13 +20,23 @@ document.getElementById('app-01').innerHTML = compiled01(data01);
 // - It can be a good practice to include Helpers in a seperate file
 
 var helpers = {
-    exampleHelper: function (x) { 
+    exampleHelper: function (x) {
         // this keyword refers to the context where helper was called
         return x;
+    },
+    safeHelper: function () {
+        // can render non escaped html with this constructor
+        return new Handlebars.SafeString('<i>foo</i>');
+    },
+    escapeHelper: function () { 
+        // this will escape the html code so browsers dont render it
+        return Handlebars.escapeExpression('<i>foo</i>');
     }
 };
 
 Handlebars.registerHelper('exampleHelper', helpers.exampleHelper);
+Handlebars.registerHelper('safeHelper', helpers.safeHelper);
+Handlebars.registerHelper('escapeHelper', helpers.escapeHelper);
 
 var template02 = '{{exampleHelper "foo"}}'; // can use space seperate multiple arguments
 var compiled02 = Handlebars.compile(template02);
@@ -34,3 +44,12 @@ document.getElementById('app-02').innerHTML = compiled02();
 
 // Can use helpers from js code also
 console.log(Handlebars.helpers.exampleHelper('bar'));
+
+var template03 = '{{safeHelper}}';
+var compiled03 = Handlebars.compile(template03);
+document.getElementById('app-03').innerHTML = compiled03();
+
+// if we use {{}} handlebars will escape again, so we use {{{}}} to display the escaped html
+var template04 = '{{{escapeHelper}}}';
+var compiled04 = Handlebars.compile(template04);
+document.getElementById('app-04').innerHTML = compiled04();
