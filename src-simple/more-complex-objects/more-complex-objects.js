2c53485967fe299a687f01a05dc92778ea757bd9
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-08-02 12:19:54 +0200

    -with helper

--- src-simple/more-complex-objects/more-complex-objects.js
+++ src-simple/more-complex-objects/more-complex-objects.js
@@ -1,5 +1,5 @@
-var template = 'My name is {{name}} and I live in {{address.city}} on {{address.street}}';
-var compiled = Handlebars.compile(template);
+var template01 = 'My name is {{name}} and I live in {{address.city}} on {{address.street}}';
+var compiled01 = Handlebars.compile(template01);
 var data = {
     name: 'Foo',
     address: {
@@ -7,6 +7,13 @@ var data = {
         street: 'Somestreet'
     }
 };
-var rendered = compiled(data);
-var $app = document.getElementById('app');
-$app.innerHTML = rendered;
+var rendered01 = compiled01(data);
+var $app = document.getElementById('app-01');
+$app.innerHTML = rendered01;
+
+
+// Instead of going into the object like address.city, we can use the with Block Helper
+
+var template02 = 'My name is {{name}} and I live in {{#with address}} {{city}} on {{street}} {{/with}}';
+var compiled02 = Handlebars.compile(template02);
+document.getElementById('app-02').innerHTML = compiled02(data);
