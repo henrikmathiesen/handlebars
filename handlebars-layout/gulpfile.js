40fb8f063a89de565152520e0c3ac842f419c418
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-08-08 12:39:43 +0200

    -npm scripts -build handlebars templates to html files

--- handlebars-layout/gulpfile.js
+++ handlebars-layout/gulpfile.js
@@ -1,20 +1,23 @@
 // following along: https://cloudfour.com/thinks/the-hidden-power-of-handlebars-partials/
 
+var gulp = require('gulp');
 const handlebars = require('gulp-compile-handlebars');
 const rename = require('gulp-rename');
 
 var config = {
     path: {
-        src: './handlebars-layout/src',
-        bld: './handlebars-layout/build'
+        // The current working directory is set from gulp script in package json, so these paths are relative to that
+        src: './src',
+        bld: './build'
     }
 };
 
-gulp.task('html', function () {
+gulp.task('default', function () {
     return gulp.src(config.path.src + '/pages/*.hbs')
 
         // In the first argument (currently an empty object), we can send in template data
         .pipe(handlebars({}, {
+            // ignores included partials that are not in batch path(?)
             ignorePartials: true,
             batch: [config.path.src + '/partials']
         }))
